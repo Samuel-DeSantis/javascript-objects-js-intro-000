e50336ad8ba6d5c67f038ef43c4ac4df53846006
Samuel DeSantis <samuel.i.desantis@gmail.com>
2020-07-15 03:30:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,5 +1,3 @@
-var playlist = {
-  "Billy Joel" : "Piano Man"
-}
+var playlist = {"Billy Joel" : "Piano Man"}
 function updatePlaylist(playlist, artistName, songTitle) {return playlist[artistName] = songTitle;}
 function removeFromPlaylist(playlist, artistName) {(delete playlist[artistName] ? playlist : pass);}
